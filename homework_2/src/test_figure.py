76a3cc76caad22dbbe44422fd85c622a61b640f9
alinaermilova17 <79274509+alinaermilova17@users.noreply.github.com>
2026-04-01 12:13:45 +0300

    homework_2

---
+++ homework_2/src/test_figure.py
@@ -0,0 +1,9 @@
+from homework_2.src.circle import Circle
+from homework_2.src.rectangle import Rectangle
+
+rectangle=Rectangle(10,5)
+circle=Circle(10)
+print(circle.add_area(rectangle))
+
+print(rectangle.area)
+
